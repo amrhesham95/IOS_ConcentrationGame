0652d76eb53c85a9af6384135660f1a12f4b9dbd
Esraa Hassan <esraa.eidhassan@gmail.com>
2019-05-10 10:54:54 -0700

    first video

--- ConcentrationGame/ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ConcentrationGame/ViewController.swift
@@ -9,17 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var emojiesArray=["👻","🎃","👻","🎃"]
+    @IBOutlet var cardButtons: [UIButton]!
+    var flipCount=0{
+        didSet{
+            flipLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber=cardButtons!.index(of: sender){
+            flipCard(withEmoji: emojiesArray[cardNumber], on: sender)
+        }else{
+            print("optional not set!")
+        }
+        //flipCard(withEmoji: "👻", on: sender)
+        
+        
     }
-
-
+   
+   
+    
+    @IBOutlet weak var flipLabel: UILabel!
+    func flipCard(withEmoji emoji:String,on button:UIButton){
+        flipCount += 1
+        if(button.currentTitle == emoji){
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor=#colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor=#colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
 }
 
